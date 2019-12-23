9091b5738d5667c2474c199e97d8186d23080d43
8Tats <58597224+8Tats@users.noreply.github.com>
2019-12-23 17:35:22 +0600

    Add files via upload

---
+++ alberto/alberto/slider/main.js
@@ -0,0 +1,21 @@
+let nextBtn = document.getElementById('next'),
+    prevBtn = document.getElementById('prev'),
+    nextBtnActive = 0;
+    prevBtnActive = -640;
+
+nextBtn.onclick = function() {
+  let slides = document.querySelector('.slides');
+  nextBtnActive -= 128;
+  if (nextBtnActive <= -512) {
+    nextBtnActive = 0;
+  }
+  slides.style.left = nextBtnActive + 'px';
+}
+prevBtn.onclick = function() {
+  let slides = document.querySelector('.slides');
+  prevBtnActive += 128;
+  if (prevBtnActive > 0) {
+    prevBtnActive = -512;
+  }
+  slides.style.left = prevBtnActive + 'px';
+}
